a1b8aa72a62697a07e5144f9e8d63148d6186517
matyi <motherfucker2012@logiker.hu>
2026-01-21 10:53:50 +0100

    haskll negy

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,3 +1,5 @@
 fizzbuzz :: Int -> String
 fizzbuzz n | n `mod` 15 == 0 = "FizzBuzz"
            | n `mod` 3 == 0  = "Fizz"
+           | n `mod` 5 == 0  = "Buzz"
+           | otherwise       = show n
